9782b08a887515d8712906a19af02585c6f99fad
shivgitcode <shivneeraj2004@gmail.com>
2024-12-08 14:51:42 +0530

    done changes in index.js

--- server/index.js
+++ server/index.js
@@ -10,9 +10,7 @@ const cors = require("cors");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-app.use(cookieParser());
 app.use(express.json());
-
 app.use(
   cors({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
@@ -20,6 +18,7 @@ app.use(
     origin: "*",
   })
 );
+app.use(cookieParser());
 
 app.use("/api/v1", userRouter, penRouter);
 
