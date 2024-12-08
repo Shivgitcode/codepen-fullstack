fd53bb88826357f294c5a4a81253e7eb7492f695
shivgitcode <shivneeraj2004@gmail.com>
2024-12-08 14:35:09 +0530

    done changes in index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const cors = require("cors");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+app.use(cookieParser());
+app.use(express.json());
 app.use(
   cors({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
@@ -17,9 +19,6 @@ app.use(
     origin: "https://codepen-fullstack-client.vercel.app",
   })
 );
-
-app.use(cookieParser());
-app.use(express.json());
 
 app.use("/api/v1", userRouter, penRouter);
 
